5a6e202e49769b917915ea817c5df773cb38344e
Cristian Arabaolaza <carabaolaza@sgfintech.com.ar>
2026-03-27 16:04:01 -0300

    Implement Two Sum with hash map and tests

---
+++ Algorithms/Arrays/TwoSumSolver.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.Arrays;
+
+public class TwoSumSolver
+{
+    public int[] Solve(int[] nums, int target)
+    {
+        var dict = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            int complement = target - nums[i];
+
+            if (dict.ContainsKey(complement))
+                return new[] { dict[complement], i };
+
+            dict[nums[i]] = i;
+        }
+
+        throw new ArgumentException();
+    }
+}
---
+++ AlgorithmsTests/Arrays/TwoSumTests.cs
@@ -0,0 +1,26 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+
+namespace Algorithms.Tests.Arrays;
+
+public class TwoSumTests
+{
+    [Fact]
+    public void Solve_ShouldReturnIndexes_WhenSolutionExists()
+    {
+        var solver = new TwoSumSolver();
+
+        var result = solver.Solve(new[] { 2, 7, 11, 15 }, 9);
+
+        result.Should().BeEquivalentTo(new[] { 0, 1 });
+    }
+
+    [Fact]
+    public void Solve_ShouldThrowException_WhenNoSolution()
+    {
+        var solver = new TwoSumSolver();
+        var nums = new[] { 1, 2, 3 };
+
+        Assert.Throws<ArgumentException>(() => solver.Solve(nums, 10));
+    }
+}

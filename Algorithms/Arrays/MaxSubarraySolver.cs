d231de4e63f0ca86b00c59ecc3d01b6b601967c9
Cristian Arabaolaza <carabaolaza@sgfintech.com.ar>
2026-03-27 17:35:32 -0300

    Add Kadane algorithm (max subarray) with tests

---
+++ Algorithms/Arrays/MaxSubarraySolver.cs
@@ -0,0 +1,18 @@
+﻿namespace Algorithms.Arrays;
+
+public class MaxSubarraySolver
+{
+    public int Solve(int[] nums)
+    {
+        int max = nums[0];
+        int current = nums[0];
+
+        for (int i = 1; i < nums.Length; i++)
+        {
+            current = Math.Max(nums[i], current + nums[i]);
+            max = Math.Max(max, current);
+        }
+
+        return max;
+    }
+}
---
+++ AlgorithmsTests/Arrays/MaxSubarrayTests.cs
@@ -0,0 +1,16 @@
+﻿using Algorithms.Arrays;
+using FluentAssertions;
+
+namespace Algorithms.Tests.Arrays;
+
+public class MaxSubarrayTests
+{
+    [Fact]
+    public void Solve_ShouldReturnMaxSum()
+    {
+        var solver = new MaxSubarraySolver();
+
+        solver.Solve(new[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 })
+              .Should().Be(6);
+    }
+}

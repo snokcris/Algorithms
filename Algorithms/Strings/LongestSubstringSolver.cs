f44712e137e40adff628996d9dd09f7de337b646
Cristian Arabaolaza <carabaolaza@sgfintech.com.ar>
2026-03-27 17:39:01 -0300

    Add longest substring without repeating characters and test

---
+++ Algorithms/Strings/LongestSubstringSolver.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms.Strings;
+
+public class LongestSubstringSolver
+{
+    public int Solve(string s)
+    {
+        var set = new HashSet<char>();
+        int left = 0, max = 0;
+
+        for (int right = 0; right < s.Length; right++)
+        {
+            while (set.Contains(s[right]))
+            {
+                set.Remove(s[left]);
+                left++;
+            }
+
+            set.Add(s[right]);
+            max = Math.Max(max, right - left + 1);
+        }
+
+        return max;
+    }
+}
---
+++ AlgorithmsTests/Strings/LongestSubstringTests.cs
@@ -0,0 +1,15 @@
+﻿using Algorithms.Strings;
+using FluentAssertions;
+
+namespace Algorithms.Tests.Strings;
+
+public class LongestSubstringTests
+{
+    [Fact]
+    public void Solve_ShouldReturnLength()
+    {
+        new LongestSubstringSolver()
+            .Solve("abcabcbb")
+            .Should().Be(3);
+    }
+}
